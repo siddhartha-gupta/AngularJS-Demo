8ee7d7b69365ac4edcca5ae775b1974706d532fe
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2015-09-12 22:53:27 +0530

    event triggering

--- books/js/config.js
+++ books/js/config.js
@@ -26,4 +26,10 @@ booksApp.config(function($routeProvider, $locationProvider) {
 	}).otherwise({
 		redirectTo: '/books'
 	});
+}).run(function($rootScope, $location) {
+	$rootScope.$on("$routeChangeSuccess", function(event, next, current) {
+		console.log('routeChangeSuccess: ', next, current);
+		// $rootScope.$broadcast will notify all $rootScope.$on as well as $scope.$on listeners
+		$rootScope.$broadcast('routeChangeSuccessEvent', next, current);
+	});
 });
--- books/js/controllers/headerController.js
+++ books/js/controllers/headerController.js
@@ -1,7 +1,22 @@
-booksApp.controller('headerController', function headerController($scope) {
+booksApp.controller('headerController', function headerController($scope, $location) {
 	$scope.data = {
 		'imageUrl': 'img/angularjs-logo.png',
 	};
+	$scope.show = false;
+
+	$scope.$on('routeChangeSuccessEvent', function(event, next, current) {
+		console.log('routeChangeSuccessEvent captured: ', next, current);
+
+		switch (next.$$route.controller) {
+			case 'mainController':
+				$scope.show = false;
+				break;
+
+			case 'bookController':
+				$scope.show = true;
+				break;
+		}
+	});
 
 	$scope.goBack = function() {
 		console.log('go back clicked');
