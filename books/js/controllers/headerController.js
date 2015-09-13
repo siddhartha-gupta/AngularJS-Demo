16b6e8b09e27dd3a479b0ef16acca1404215ffed
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2015-09-13 13:24:19 +0530

    to reset app

--- books/js/controllers/headerController.js
+++ books/js/controllers/headerController.js
@@ -1,9 +1,10 @@
-booksApp.controller('headerController', function headerController($scope, $location) {
+booksApp.controller('headerController', function headerController($scope, $location, localStorageService, $window) {
 	$scope.data = {
 		'imageUrl': 'img/angularjs-logo.png',
 	};
-	$scope.show = false;
+	$scope.showBackBtn = false;
 	$scope.isDisabled = true;
+	$scope.showResetBtn = true;
 
 	$scope.$on('routeChangeStartEvent', function(event, next, current) {
 		switch (next.$$route.controller) {
@@ -21,12 +22,14 @@ booksApp.controller('headerController', function headerController($scope, $locat
 		switch (next.$$route.controller) {
 			case 'mainController':
 				$scope.isDisabled = false;
-				$scope.show = false;
+				$scope.showBackBtn = false;
+				$scope.showResetBtn = true;
 				break;
 
 			case 'bookController':
 				$scope.isDisabled = false;
-				$scope.show = true;
+				$scope.showBackBtn = true;
+				$scope.showResetBtn = true;
 				break;
 		}
 	});
@@ -43,7 +46,15 @@ booksApp.controller('headerController', function headerController($scope, $locat
 		}
 	});
 
-	$scope.goBack = function() {
+	$scope.resetApp = function(event) {
+		event.preventDefault();
+		localStorageService.clearAll();
+		$location.path('/books');
+		$window.location.reload();
+	};
+
+	$scope.goBack = function(event) {
+		event.preventDefault();
 		$location.path('/books');
 	};
 });
