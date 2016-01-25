f15a4d3d8f3a993a30bb5e7b7af011aa7f17daa9
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-01-25 16:32:12 +0530

    displaying data slowely

--- typescript2/app/app.component.ts
+++ typescript2/app/app.component.ts
@@ -75,7 +75,7 @@ export class AppComponent implements OnInit {
 	public selectedHero: Hero;
 	constructor(private _heroService: HeroService) { }
 	getHeroes() {
-		this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+		this._heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
 	}
 	ngOnInit() {
 		this.getHeroes();
