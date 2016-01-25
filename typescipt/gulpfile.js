5fc9426fb576c7da27545a66343d6324be7a2857
Siddhartha Gupta <siddharath.gupta@pgi.com>
2016-01-25 14:29:43 +0530

    Revert "updating files"
    This reverts commit 82ff7e44e24c69e332e9b0a530a2e90900c25422.

--- typescipt/gulpfile.js
+++ typescipt/gulpfile.js
@@ -2,47 +2,11 @@
 var gulp = require('gulp'),
 	ts = require('gulp-typescript');
 
-
-gulp.task('setup', function(done) {
-	gulp.src([
-		'node_modules/angular2/bundles/js',
-		'node_modules/angular2/bundles/angular2.*.js*',
-		'node_modules/angular2/bundles/http.*.js*',
-		'node_modules/angular2/bundles/router.*.js*',
-		'node_modules/es6-shim/es6-shim.js*',
-		'node_modules/systemjs/dist/*.*',
-		'node_modules/jquery/dist/jquery.*js',
-		'node_modules/bootstrap/dist/js/bootstrap*.js',
-		'node_modules/@reactivex/rxjs/dist/global/Rx.js'
-	]).pipe(gulp.dest('web/lib'));
-
-	gulp.src([
-		'node_modules/bootstrap/dist/css/bootstrap.css'
-	]).pipe(gulp.dest('web/css'));
-});
-
 // replace the Gulp task for ts with this:
-
-gulp.task('assets', function() {
-	gulp.src(['./src/**/*.json',
-			'./src/**/*.html',
-			'./src/**/*.css'
-		])
-		.pipe(gulp.dest('./web'));
-});
 
 var tsProject = ts.createProject({
 	declaration: true,
 	noExternalResolve: true
-});
-
-gulp.task('watch', ['watch.assets', 'watch.ts', 'watch.web']);
-
-gulp.task('watch.assets', ['assets'], function() {
-	return gulp.watch(['./src/**/*.json',
-		'./src/**/*.html',
-		'./src/**/*.css'
-	], ['assets']);
 });
 
 gulp.task('ts', function(done) {
@@ -57,4 +21,12 @@ gulp.task('ts', function(done) {
 	return tsResult.js.pipe(gulp.dest('web/js'));
 });
 
-gulp.task('default', ['express', 'livereload', 'watch']);
+gulp.task('watch', ['ts'], function() {
+	gulp.watch([
+		"node_modules/angular2/bundles/typings/angular2/angular2.d.ts",
+		"node_modules/angular2/bundles/typings/angular2/http.d.ts",
+		"node_modules/angular2/bundles/typings/angular2/router.d.ts",
+		"node_modules/@reactivex/rxjs/dist/es6/Rx.d.ts",
+		"src/**/*.ts"
+	], ['ts']);
+});
