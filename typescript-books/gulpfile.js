fe902115f3b7f5f74097ae5ee949493dda135292
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-01-27 15:54:24 +0530

    trying to add gulp

---
+++ typescript-books/gulpfile.js
@@ -0,0 +1,78 @@
+var gulp = require('gulp'),
+	del = require('del'),
+	typescript = require('gulp-typescript'),
+	tscConfig = require('./tsconfig.json'),
+	sourcemaps = require('gulp-sourcemaps'),
+	tslint = require('gulp-tslint'),
+	browserSync = require('browser-sync'),
+	reload = browserSync.reload,
+	tsconfig = require('tsconfig-glob');
+
+// clean the contents of the distribution directory
+gulp.task('clean', function() {
+	return del('dist/**/*');
+});
+
+// copy static assets - i.e. non TypeScript compiled source
+gulp.task('copy:assets', ['clean'], function() {
+	return gulp.src(['app/**/*', 'index.html', 'styles.css', '!app/**/*.ts'], {
+			base: './'
+		})
+		.pipe(gulp.dest('dist'))
+});
+
+// copy dependencies
+gulp.task('copy:libs', ['clean'], function() {
+	return gulp.src([
+			'node_modules/es6-shim/es6-shim.min.js',
+			'node_modules/systemjs/dist/system-polyfills.js',
+			'node_modules/angular2/bundles/angular2-polyfills.js',
+			'node_modules/systemjs/dist/system.src.js',
+			'node_modules/rxjs/bundles/Rx.js',
+			'node_modules/angular2/bundles/angular2.dev.js',
+			'node_modules/angular2/bundles/http.dev.js',
+			'node_modules/ng2-bootstrap/bundles/ng2-bootstrap.min.js'
+		])
+		.pipe(gulp.dest('dist/lib'))
+});
+
+// linting
+gulp.task('tslint', function() {
+	return gulp.src('app/**/*.ts')
+		.pipe(tslint())
+		.pipe(tslint.report('verbose'));
+});
+
+
+// TypeScript compile
+gulp.task('compile', ['clean'], function() {
+	return gulp
+		.src(tscConfig.files)
+		.pipe(sourcemaps.init())
+		.pipe(typescript(tscConfig.compilerOptions))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('dist/app'));
+});
+
+// update the tsconfig files based on the glob pattern
+gulp.task('tsconfig-glob', function() {
+	return tsconfig({
+		configPath: '.',
+		indent: 2
+	});
+});
+
+// Run browsersync for development
+gulp.task('serve', ['build'], function() {
+	browserSync({
+		server: {
+			baseDir: 'dist'
+		}
+	});
+
+	gulp.watch(['app/**/*', 'index.html', 'styles.css'], ['buildAndReload']);
+});
+
+gulp.task('build', ['tslint', 'compile', 'copy:libs', 'copy:assets']);
+gulp.task('buildAndReload', ['build'], reload);
+gulp.task('default', ['build']);
