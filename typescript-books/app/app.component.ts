55806f99e3107a4b4b99846d5be6c2157d85730a
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-01-27 14:00:21 +0530

    showing google books data

--- typescript-books/app/app.component.ts
+++ typescript-books/app/app.component.ts
@@ -12,31 +12,27 @@ import {api} from './api.service'
 	<h1>{{title}}</h1>
 	<ul class="thumbnails list-unstyled">
 		<li *ngFor="#bookData of booksData">
-			<img class="pull-left img-size" src="{{bookData.imgsrc}}" />
+			<img class="pull-left img-size" src="{{bookData.volumeInfo.imageLinks.smallThumbnail}}" />
 			<div class="pull-left book-info">
-				<h4 class="well-title">{{bookData.title}}</h4>
-				<strong>Publisher: </strong><span>{{bookData.publisher}}</span><br/>
+				<h4 class="well-title">{{bookData.volumeInfo.title}}</h4>
+				<strong>Publisher: </strong><span>{{bookData.volumeInfo.publisher}}</span><br/>
 			</div>
         </li>
 	</ul>
-  `,
-	viewProviders: [HTTP_PROVIDERS]
+  `
 })
 
 export class App {
-	// We inject the created router via DI
 	public title = 'Google Books API';
 	public booksData: Array<Object>;
 
-	constructor(public api: api) {
-	}
+	constructor(public api: api) {}
 
 	ngOnInit() {
-		this.api.getData('https://www.googleapis.com/books/v1/volumes?q=test').subscribe(function(data) {
-			console.log('data: ', data);
-			this.booksData = data;
-
-			console.log('this.booksData: ', this.booksData);
-		});
+		this.api.getData('https://www.googleapis.com/books/v1/volumes?q=test').subscribe(
+			data => this.booksData = data.items,
+			error => console.error('Error: ' + err),
+			() => console.log('Completed!')
+		);
 	}
 }
