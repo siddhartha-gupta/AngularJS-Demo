b146809947d6e3d611dd9395c69cc506975bb20b
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-01-28 15:08:50 +0530

    price and click function

--- typescript-books/app/app.component.ts
+++ typescript-books/app/app.component.ts
@@ -17,6 +17,15 @@ import {api} from './api.service'
 			<div class="pull-left book-info">
 				<h4 class="well-title">{{bookData.volumeInfo.title}}</h4>
 				<strong>Publisher: </strong><span>{{bookData.volumeInfo.publisher}}</span><br/>
+				<strong>Price: </strong>				
+				<span *ngIf="bookData.saleInfo.saleability === 'FOR_SALE'">
+					Rs. {{bookData.saleInfo.listPrice.amount}}
+				</span>
+				<span *ngIf="bookData.saleInfo.saleability === 'NOT_FOR_SALE'">
+					Not available
+				</span>
+				<br/>
+				<a href="#" (click)="showbook($event, bookData)">Read more...</a>
 			</div>
         </li>
 	</ul>
@@ -32,7 +41,16 @@ export class App {
 		this.api.getData('https://www.googleapis.com/books/v1/volumes?q=test').subscribe(
 			data => this.booksData = data.items,
 			error => console.error('Error: ' + error),
-			() => console.log('Completed!')
+			() => console.log('Completed!: ', this.booksData);
 		);
+	}
+
+	showbook(event, data) {
+		event.preventDefault();
+		event.stopPropagation();
+
+		console.log(data.id);
+		// let id = $scope.id;
+		// $scope.showbookfn(id);
 	}
 }
