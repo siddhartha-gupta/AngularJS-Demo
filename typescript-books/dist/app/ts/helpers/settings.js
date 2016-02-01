bce956be95e2d246876b1e2b46ef5b0b5a024378
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-02-01 19:26:51 +0530

    adding compiled files

---
+++ typescript-books/dist/app/ts/helpers/settings.js
@@ -0,0 +1,13 @@
+System.register([], function(exports_1) {
+    var _settings;
+    return {
+        setters:[],
+        execute: function() {
+            exports_1("_settings", _settings = {
+                buildPath: 'app/templates/'
+            });
+        }
+    }
+});
+
+//# sourceMappingURL=settings.js.map
