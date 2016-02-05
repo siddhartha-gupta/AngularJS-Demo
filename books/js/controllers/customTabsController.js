1b23724d8f20fbcab2273854d9d6ff61b5af4c10
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-02-05 14:40:50 +0530

    working on local storage

--- books/js/controllers/customTabsController.js
+++ books/js/controllers/customTabsController.js
@@ -1,12 +1,13 @@
 'use strict';
 
-booksApp.controller('customTabs', function($scope) {
+booksApp.controller('customTabs', function($scope, localStorageService) {
 	$scope.activeTab = 'All';
 
 	$scope.toggleActive = function(ind) {
 		angular.forEach($scope.ngModel, function(value, key) {
 			if (key == ind) {
 				$scope.activeTab = key;
+				localStorageService.set('activeTab', key);
 			}
 		});
 	};
