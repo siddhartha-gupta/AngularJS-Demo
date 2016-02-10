f0fccf223fa0faf9dc57cd8fe564a0325f0dc88c
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-02-10 12:08:54 +0530

    cleanup

--- tic-tac-toe-node-server/app.js
+++ tic-tac-toe-node-server/app.js
@@ -19,47 +19,20 @@ io.sockets.on('connection', function(socket) {
 	});
 
 	socket.on('get-players-list', function(data) {
-		var playersList = getPlayersList();
-		socket.emit('current-players-list', playersList);
+		getPlayersList(socket);
 	});
 
 	socket.on('add-recipient', function(data) {
-		var resp = '';
-
-		if (clients.hasOwnProperty(data.emailId)) {
-			clients[data.emailId].recipient = data.recipient;
-			clients[data.recipient].recipient = data.emailId;
-			console.log(clients);
-			resp = 'Recipient added successfully';
-		} else {
-			resp = 'Error in adding recipient';
-		}
-		// Send resp back to user
-		// socket.emit('add-recipient-resp', resp);
-		io.sockets.connected[clients[data.recipient].socket].emit('add-recipient-resp', data.emailId);
-		io.sockets.connected[clients[data.emailId].socket].emit('add-recipient-resp', data.recipient);
+		addRecipient(data);
 	});
 
 	socket.on('send-message', function(data) {
-		console.log("time to send msg: ", data);
-		console.log("clients: ", clients);
-
-		if (clients[data.recipient]) {
-			console.log('sending to: ', clients[data.recipient]);
-			io.sockets.connected[clients[data.recipient].socket].emit('send-message-resp', data.msg);
-		} else {
-			console.log("User does not exist: " + data.recipient);
-		}
+		sendMessage(data);
 	});
 
 	//Removing the socket on disconnect
 	socket.on('disconnect', function() {
-		for (var name in clients) {
-			if (clients[name].socket === socket.id) {
-				delete clients[name];
-				break;
-			}
-		}
+		disconnectClient(socket);
 	})
 });
 
@@ -74,7 +47,7 @@ function registerEmail(socket, data) {
 		};
 		console.log(clients);
 		resp = 'Email registered successfully';
-		players = getPlayersList();
+		players = providePlayersList();
 
 		io.sockets.emit('current-players-list', players);
 	} else {
@@ -84,7 +57,7 @@ function registerEmail(socket, data) {
 	socket.emit('register-email-resp', resp);
 }
 
-function getPlayersList() {
+function providePlayersList() {
 	var list = [];
 	for (var key in clients) {
 		list.push({
@@ -93,4 +66,44 @@ function getPlayersList() {
 		});
 	}
 	return list;
+}
+
+function getPlayersList(socket) {
+	var playersList = providePlayersList();
+	socket.emit('current-players-list', playersList);
+}
+
+function addRecipient(data) {
+	var resp = '';
+
+	if (clients.hasOwnProperty(data.emailId)) {
+		clients[data.emailId].recipient = data.recipient;
+		clients[data.recipient].recipient = data.emailId;
+		console.log(clients);
+		resp = 'Recipient added successfully';
+	} else {
+		resp = 'Error in adding recipient';
+	}
+	io.sockets.connected[clients[data.recipient].socket].emit('add-recipient-resp', data.emailId);
+	io.sockets.connected[clients[data.emailId].socket].emit('add-recipient-resp', data.recipient);
+}
+
+function sendMessage(data) {
+	console.log("time to send msg: ", data);
+
+	if (clients[data.recipient]) {
+		console.log('sending to: ', clients[data.recipient]);
+		io.sockets.connected[clients[data.recipient].socket].emit('send-message-resp', data.msg);
+	} else {
+		console.log("User does not exist: " + data.recipient);
+	}
+}
+
+function disconnectClient(socket) {
+	for (var name in clients) {
+		if (clients[name].socket === socket.id) {
+			delete clients[name];
+			break;
+		}
+	}
 }
