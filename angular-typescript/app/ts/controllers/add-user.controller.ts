ab88ad1d5946a56a277d50f1701b7834eaf2df41
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-02-23 10:50:09 +0530

    showing errors

--- angular-typescript/app/ts/controllers/add-user.controller.ts
+++ angular-typescript/app/ts/controllers/add-user.controller.ts
@@ -7,6 +7,7 @@ module app {
 		private validEmail: Boolean;
 		private userdata: UserDataInterface;
 		private appConfig: appConfigInterface;
+		private modalDialogue: ModalDialogueInterface;
 
 		public static $inject = [
 			'$scope',
@@ -27,13 +28,8 @@ module app {
 
 			this.appConfig = app.Constants.Default;
 			this.validEmail = false;
-			this.userdata = {
-				'firstname': '',
-				'lastname': '',
-				'email': '',
-				'phonenumber': '',
-				'location': 'IN'
-			};
+			this.userDataDefault();
+			this.modalDialogueDefault();
 		}
 
 		validateEmail(val: string) {
@@ -46,21 +42,90 @@ module app {
 		}
 
 		validateForm() {
-
+			// make null undefined checks here
+			return true;
 		}
 
 		addUser() {
 			this.$log.log('add user: ', this.userdata);
 
-			this.apiService.postCall({
-				'url': this.appConfig.serverUrl + 'adduser',
-				data: this.userdata
-			}).success((response) => {
-				this.$log.log('success: ', response);
-				this.$location.path('/userslist').replace();
-			}).error((response) => {
-				this.$log.log('error: ', response);
-			});
+			if (this.validateForm()) {
+				this.apiService.postCall({
+					'url': this.appConfig.serverUrl + 'adduser',
+					data: this.userdata
+				}).success((response: any) => {
+					this.$log.log('success: ', response);
+
+					if (response && response.resp && response.resp === 'Email already in use') {
+						this.showModalDialogue('emailInUse');
+					} else {
+						this.$location.path('/userslist').replace();
+					}
+				}).error((response: any) => {
+					this.$log.log('error: ', response);
+				});
+			}
+		}
+
+		userDataDefault() {
+			this.userdata = {
+				'firstname': '',
+				'lastname': '',
+				'email': '',
+				'phonenumber': '',
+				'location': 'IN'
+			};
+		}
+		showModalDialogue(errorType: string) {
+			let title: string = '',
+				body: string = '';
+
+			console.log('showModalDialogue: ', errorType);
+			switch (errorType) {
+				case 'emailInUse':
+					title = 'Email already in use';
+					body = 'Email ID is already in use, please enter a unique Email address';
+					break;
+
+				case 'inValidForm':
+					title = 'Incomplete form';
+					body = 'Please fill the missing data in the form';
+					break;
+			}
+
+			this.modalDialogue = {
+				isVisible: true,
+				title: title,
+				body: body,
+				btn1Txt: 'Ok',
+				btn2Txt: '',
+				showBtn2: false,
+				btn1Callback: this.hideModalDialogue.bind(this),
+				btn2Callback: function() { },
+				closeBtnCallback: this.hideModalDialogue.bind(this),
+			};
+		}
+
+		hideModalDialogue(event?: Event) {
+			if (event) {
+				event.stopPropagation();
+				event.preventDefault();
+			}
+			this.modalDialogueDefault();
+		}
+
+		modalDialogueDefault() {
+			this.modalDialogue = {
+				isVisible: false,
+				title: '',
+				body: '',
+				btn1Txt: '',
+				btn2Txt: '',
+				showBtn2: false,
+				btn1Callback: function() { },
+				btn2Callback: function() { },
+				closeBtnCallback: function() { },
+			};
 		}
 	}
 }
--- angular-typescript/dist/definitions/app/ts/controllers/add-user.controller.d.ts
+++ angular-typescript/dist/definitions/app/ts/controllers/add-user.controller.d.ts
@@ -8,10 +8,15 @@ declare module app {
         private validEmail;
         private userdata;
         private appConfig;
+        private modalDialogue;
         static $inject: string[];
         constructor($scope: ng.IScope, $location: ng.ILocationService, $log: ng.ILogService, apiService: APIService);
         validateEmail(val: string): void;
-        validateForm(): void;
+        validateForm(): boolean;
         addUser(): void;
+        userDataDefault(): void;
+        showModalDialogue(errorType: string): void;
+        hideModalDialogue(event?: Event): void;
+        modalDialogueDefault(): void;
     }
 }
