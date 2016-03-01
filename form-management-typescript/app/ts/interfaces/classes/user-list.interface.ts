7b0f98ceed71bf3be95ee6abadf208ccebc2de5d
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-03-01 11:18:00 +0530

    adding/modifying interfaces

--- form-management-typescript/app/ts/interfaces/classes/user-list.interface.ts
+++ form-management-typescript/app/ts/interfaces/classes/user-list.interface.ts
@@ -3,16 +3,25 @@
 module app {
 	'use strict';
 	export interface UsersListInterface {
-		getUsers()
-		processServerData(data: any)
-		addUser()
-		editUserClick(key: string)
-		updateUserData(data: any, userId: string)
-		hideEditPopup(event?: Event)
-		editUserDefault()
-		deleteUserClick(key: string)
-		deleteUserConfirm(key: string)
-		hideModalDialogue(event?: Event)
-		modalDialogueDefault()
+		getUsers(): void;
+		processServerData(data: any): void;
+		addUser(): void;
+		actionHandler(type: string, userId: string, userData?: any): void;
+		validateEmail(val: string): void;
+		editUserClick(userId: string): void;
+		updateUserData(data: any, userId: string): void;
+		onUserUpdateResp(resp: Boolean): void;
+		deleteUserClick(key: string): void;
+		deleteUserConfirm(key: string): void;
+		onUserDeleted(resp: Boolean): void;
+		hideEditPopup(event?: Event): void;
+		hideModalDialogue(event?: Event): void;
+		manageSortOrder(orderBy: any): void;
+		showInfoSlider(params: any): void;
+		hideInfoSlider(): void;
+		createtableHeading(): void;
+		editUserDefault(): void;
+		modalDialogueDefault(): void;
+		infoSliderDefault(): void;
 	}
 }
--- form-management-typescript/dist/definitions/app/ts/interfaces/classes/user-list.interface.d.ts
+++ form-management-typescript/dist/definitions/app/ts/interfaces/classes/user-list.interface.d.ts
@@ -1,16 +1,25 @@
 /// <reference path="../../../_all.d.ts" />
 declare module app {
     interface UsersListInterface {
-        getUsers(): any;
-        processServerData(data: any): any;
-        addUser(): any;
-        editUserClick(key: string): any;
-        updateUserData(data: any, userId: string): any;
-        hideEditPopup(event?: Event): any;
-        editUserDefault(): any;
-        deleteUserClick(key: string): any;
-        deleteUserConfirm(key: string): any;
-        hideModalDialogue(event?: Event): any;
-        modalDialogueDefault(): any;
+        getUsers(): void;
+        processServerData(data: any): void;
+        addUser(): void;
+        actionHandler(type: string, userId: string, userData?: any): void;
+        validateEmail(val: string): void;
+        editUserClick(userId: string): void;
+        updateUserData(data: any, userId: string): void;
+        onUserUpdateResp(resp: Boolean): void;
+        deleteUserClick(key: string): void;
+        deleteUserConfirm(key: string): void;
+        onUserDeleted(resp: Boolean): void;
+        hideEditPopup(event?: Event): void;
+        hideModalDialogue(event?: Event): void;
+        manageSortOrder(orderBy: any): void;
+        showInfoSlider(params: any): void;
+        hideInfoSlider(): void;
+        createtableHeading(): void;
+        editUserDefault(): void;
+        modalDialogueDefault(): void;
+        infoSliderDefault(): void;
     }
 }
