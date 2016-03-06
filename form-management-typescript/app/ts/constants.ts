355c5af5b75cf661164ae111683c8785a0031ee6
Siddhartha Gupta <makerzzz.zzzz@gmail.com>
2016-03-06 18:29:52 +0530

    changes

--- form-management-typescript/app/ts/constants.ts
+++ form-management-typescript/app/ts/constants.ts
@@ -7,7 +7,7 @@ module app {
 		static get Default(): any {
 			return {
 				// serverUrl: 'http://localhost:8080/',
-				serverUrl: 'https://user-management-881512.herokuapp.com',
+				serverUrl: 'https://user-management-881512.herokuapp.com/',
 				templateUrl: '../templates/'
 			}
 		}
